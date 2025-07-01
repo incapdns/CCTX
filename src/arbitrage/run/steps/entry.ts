306e62d1bd85166e8ac3a214b54e2e6dd5828ebd
Lucas Paixão <lucas.paixao@suprasoft.net>
2025-07-01 07:47:11 -0300

    Remove average order filtering logic from entry and exit arbitrage steps for simplification

--- src/arbitrage/run/steps/entry.ts
+++ src/arbitrage/run/steps/entry.ts
@@ -1,5 +1,4 @@
 import { Order } from 'ccxt';
-import Decimal from 'decimal.js';
 import { Exchange } from "../../../exchange";
 import { doArbitrage } from '../../compute';
 import { ArbitrageDirection } from "../../compute/common";
@@ -60,32 +59,6 @@ export const runEntryArbitrage = async ({
     step.spot!.result!,
     step.future!.result!
   ]
-
-  const spotOrdersAvg = Decimal(
-    spotBook
-      .asks
-      .reduce((previous, [_, quantity]) => previous + quantity, 0)
-  )
-    .div(spotBook.asks.length)
-    .mul(0.75)
-    .toNumber()
-
-  const futureOrdersAvg = Decimal(
-    futureBook
-      .bids
-      .reduce((previous, [_, quantity]) => previous + quantity, 0)
-  )
-    .div(futureBook.bids.length)
-    .mul(0.75)
-    .toNumber()
-
-  spotBook.asks = spotBook
-    .asks
-    .filter(([_, quantity]) => quantity >= spotOrdersAvg)
-
-  futureBook.bids = futureBook
-    .bids
-    .filter(([_, quantity]) => quantity >= futureOrdersAvg)
 
   const spotMarket = manager.market(symbol)
   const futureMarket = manager.market(`${symbol}:USDT`)
